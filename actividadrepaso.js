b7f82da3ea9a69fd0cb3974be8380148b5b2689e
Salahddin123 <aemlocal@ED05000701M063.educa.jcyl.es>
2024-12-13 12:55:29 +0100

    Javascript2

---
+++ actividadrepaso.js
@@ -0,0 +1,9 @@
+function ocultarImagen() {
+    document.getElementById('miImagen').style.display = 'none';
+}
+
+function cambiarImagen() {
+    const imagen = document.getElementById('miImagen');
+    imagen.src = "https://th.bing.com/th/id/R.32722229d820bfa4193b1b925de31b0f?rik=0lH7Ch7ewXFdrQ&riu=http%3a%2f%2f2.bp.blogspot.com%2f-8CIlnWucJQM%2fUt31vxtQ-hI%2fAAAAAAACJUs%2fTOQRJ-9N54o%2fs1600%2fburro-asno-posando-para-la-c%c3%a1mara-equino-animal-de-carga-en-Nashville-TN-USA-by-Jos%c3%a9%2bLuis%2b%c3%81vila%2bHerrera.jpg&ehk=VJisyNbE54jJ%2bU%2bEKwylt7XXyOmMtucfHc0GYhcFz54%3d&risl=&pid=ImgRaw&r=0";
+}
+
